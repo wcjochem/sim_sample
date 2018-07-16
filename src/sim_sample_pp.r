31e0cce69384d5164a4bd0051c399c77251cd117
wcjochem <7605755+wcjochem@users.noreply.github.com>
2018-07-16 16:58:06 +0100

    moved helper function

--- src/sim_sample_pp.r
+++ src/sim_sample_pp.r
@@ -96,16 +96,6 @@ beta1 <- 3 # elevation
 beta2 <- -1.5 # quadratic
 beta3 <- -1 # trend
 
-
-# Function to draw samples from a multivariate normal distribution
-rmvn <- function(n, mu = 0, V = matrix(1)) {
-  p <- length(mu)
-  if (any(is.na(match(dim(V), p)))) 
-    stop("Dimension problem!")
-  D <- chol(V)
-  t(matrix(rnorm(n * p), ncol = p) %*% D + rep(mu, rep(n, p)))
-}
-
 ## create covariate layers
 # elevation
 data("volcano") # R dataset for example
