62569a3ceb9294a603e7922498e7e29ef6161059
wcjochem <7605755+wcjochem@users.noreply.github.com>
2019-04-08 14:29:43 +0100

    load code to define settlement

--- simple_sample.r
+++ simple_sample.r
@@ -1,18 +1,12 @@
 # 
 # Simulate spatially-correlated density maps
-# sim_sample_test.r
+# sim_sample.r
 #
 # Demonstration program for simulating spatially-correlated data
 # and testing a range of sampling techniques to estimate population.
 #
 # Author: Chris Jochem (W.C.Jochem@soton.ac.uk)
 #
-# Random field simulation techniques based on work by:
-# Jérôme Guélat (https://rpubs.com/jguelat/autocorr2)
-#
-# Dorazio and Karanth (2017). "A hierarchical model for estimating 
-#    the spatial distribution and abundance of animals detected 
-#    by continuous-time recorders." PLoS ONE, 12(5): e0176966.
 #
 
 library(INLA)
@@ -27,6 +21,7 @@ rm(list=ls())
 set.seed(1126)
 inla.seed <- 1126
 
+####
 # helper function to plot spatial field
 # source: https://haakonbakka.bitbucket.io/btopic108.html
 local.plot.field <- function(field, mesh, xlim=c(0,10), ylim=c(0,10), ...){
@@ -39,4 +34,6 @@ local.plot.field <- function(field, mesh, xlim=c(0,10), ylim=c(0,10), ...){
   image.plot(list(x=proj$x, y=proj$y, z=field.proj), xlim=xlim, ylim=ylim, col=plasma(n.col), nlevel=n.col+1, ...)
 }
 
+# load code to create settlement type grids
+source("./make_settlement.r")
 
