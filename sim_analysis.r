9df762f967e78793ee6a305837cd4db1bd356bc4
wcjochem <7605755+wcjochem@users.noreply.github.com>
2019-05-07 17:04:01 +0100

    get simulated layers from each file

--- sim_analysis.r
+++ sim_analysis.r
@@ -34,4 +34,20 @@ nsamples <- 100 # number of draws of each size and type
 sampleszs <- c(50, 100, 150, 200) # range of sample sizes
 
 # get list of simulated surfaces
-sim_files <- list.files("./out_sim", pattern=glob2rx("sim_surface_*.rds"), recursive=F)
+sim_files <- list.files("./out_sim", pattern=glob2rx("sim_surface_*.rds"), recursive=FALSE, full.names=TRUE)
+# loop and load all files
+for(f in sim_files){
+  print(f)
+  sim_surfaces <- readRDS(f) # load the simulated population and covariates
+  nsurfaces <- length(sim_surfaces) # number of simulated populations
+  
+  # loop each simulated surface
+  for(s in 1:nsurfaces){
+    # get simulated layers
+    truepop <- sim_surfaces[[s]]$pop
+    sett <- sim_surfaces[[s]]$sett
+    cov <- sim_surfaces[[s]]$cov
+    
+  } # end loop of simulated surfaces
+  
+} # end loop of files
