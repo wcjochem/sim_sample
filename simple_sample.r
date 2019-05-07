af7310075ffddeb2ef342a6ce71e591d09e961c4
wcjochem <7605755+wcjochem@users.noreply.github.com>
2019-05-07 15:18:30 +0100

    test looping for sim creation

--- simple_sample.r
+++ simple_sample.r
@@ -64,66 +64,66 @@ strat2 <- raster(strat2, xmn=0, xmx=20, ymn=0, ymx=20)
 # true values
 # sigma.u <- 1.5
 # range <- 3 # 1, 3, 5
-
-# make mesh
-fake.locations <- matrix(c(0,0,r_width,r_height,0,r_height,r_width,0), nrow=4, byrow=T)
-# mesh.sim <- inla.mesh.2d(loc=fake.locations, max.edge=c(0.4, 2), offset=c(1,6))
-mesh.sim <- inla.mesh.2d(loc=fake.locations, max.edge=c(0.4, 2))
-
-  plot(mesh.sim)
-  points(fake.locations, pch=16)
-  axis(1); axis(2)
-  
-# simulate field  
-spde <- inla.spde2.pcmatern(mesh.sim, prior.range=c(.5, .5), prior.sigma=c(.5, .5))
-
-Qu <- inla.spde.precision(spde, theta=c(log(range), log(sigma.u)))
-u <- inla.qsample(n=1, Q=Qu, seed=inla.seed)
-u <- u[ ,1]
-
-  local.plot.field(u, mesh.sim, xlim=c(0,r_width), ylim=c(0,r_height))
-  len <- range
-  arrows(5-0.5*len, .5, 5+0.5*len, .5, length=0.05, angle=90, code=3, lwd=3)
-  
-# simulate data at locations
-loc.data <- as.matrix(expand.grid(seq(0, r_width, .1), seq(0, r_height, .1))) + .1
-n <- nrow(loc.data)
-# create a raster covering the study area
-r <- raster(nrows=r_height, ncols=r_width, xmn=0, xmx=r_width, ymn=0, ymx=r_height, resolution=.1, crs=NULL)
-loc.data <- coordinates(r)
-n <- nrow(loc.data)
-
-# project spatial field to locations
-A <- inla.spde.make.A(mesh=mesh.sim, loc=loc.data)
-u <- drop(A %*% u)
-
-# create a covariate
-x <- runif(n)-0.5 # centred at zero
-cov_r <- r # create a blank raster
-values(cov_r) <- x # write in values
-  plot(cov_r)
-
-# construct linear predictor
-beta <- c(1, 1.5) # true coefficients 
-
-lin.pred <- beta[1] + beta[2]*x + u
-# observation
-y <- rpois(n, exp(lin.pred))
-
-y_rast <- r # store pop as raster
-values(y_rast) <- y
-  plot(y_rast) # example gridded population
-# calculate totals per area (test)
-zonal(y_rast, strat2)
-zonal(y_rast, strat1)
-sum(y)
-
-# derive a settlement "covariate" based on population centers/density
-sett <- make_settlement(y_rast)
-  plot(sett, col=randomColor(6)) # raster plotting
-  abline(v=10)
-
-zonal(y_rast, sett)  
+# 
+# # make mesh
+# fake.locations <- matrix(c(0,0,r_width,r_height,0,r_height,r_width,0), nrow=4, byrow=T)
+# # mesh.sim <- inla.mesh.2d(loc=fake.locations, max.edge=c(0.4, 2), offset=c(1,6))
+# mesh.sim <- inla.mesh.2d(loc=fake.locations, max.edge=c(0.4, 2))
+# 
+#   plot(mesh.sim)
+#   points(fake.locations, pch=16)
+#   axis(1); axis(2)
+#   
+# # simulate field  
+# spde <- inla.spde2.pcmatern(mesh.sim, prior.range=c(.5, .5), prior.sigma=c(.5, .5))
+# 
+# Qu <- inla.spde.precision(spde, theta=c(log(range), log(sigma.u)))
+# u <- inla.qsample(n=1, Q=Qu, seed=inla.seed)
+# u <- u[ ,1]
+# 
+#   local.plot.field(u, mesh.sim, xlim=c(0,r_width), ylim=c(0,r_height))
+#   len <- range
+#   arrows(5-0.5*len, .5, 5+0.5*len, .5, length=0.05, angle=90, code=3, lwd=3)
+#   
+# # simulate data at locations
+# loc.data <- as.matrix(expand.grid(seq(0, r_width, .1), seq(0, r_height, .1))) + .1
+# n <- nrow(loc.data)
+# # create a raster covering the study area
+# r <- raster(nrows=r_height, ncols=r_width, xmn=0, xmx=r_width, ymn=0, ymx=r_height, resolution=.1, crs=NULL)
+# loc.data <- coordinates(r)
+# n <- nrow(loc.data)
+# 
+# # project spatial field to locations
+# A <- inla.spde.make.A(mesh=mesh.sim, loc=loc.data)
+# u <- drop(A %*% u)
+# 
+# # create a covariate
+# x <- runif(n)-0.5 # centred at zero
+# cov_r <- r # create a blank raster
+# values(cov_r) <- x # write in values
+#   plot(cov_r)
+# 
+# # construct linear predictor
+# beta <- c(1, 1.5) # true coefficients 
+# 
+# lin.pred <- beta[1] + beta[2]*x + u
+# # observation
+# y <- rpois(n, exp(lin.pred))
+# 
+# y_rast <- r # store pop as raster
+# values(y_rast) <- y
+#   plot(y_rast) # example gridded population
+# # calculate totals per area (test)
+# zonal(y_rast, strat2)
+# zonal(y_rast, strat1)
+# sum(y)
+# 
+# # derive a settlement "covariate" based on population centers/density
+# sett <- make_settlement(y_rast)
+#   plot(sett, col=randomColor(6)) # raster plotting
+#   abline(v=10)
+# 
+# zonal(y_rast, sett)  
 
 
 ## loop to build synthetic populations ##
@@ -190,8 +190,8 @@ for (range in ranges){ # loop and vary spatial range
     y_rast <- r # store pop as raster
     values(y_rast) <- y
     
-    # derive settlement from population
-    sett <- NULL
+    # derive settlement "types" from population
+    sett <- make_settlement(y_rast)
   
     simdata[[s]] <- list("pop"=y_rast, "cov"=cov_r, "sett"=sett)
   } # end pop simulation loop
