6ec45e48f2565b833868c2bd9f7f17493cadd71b
wcjochem <7605755+wcjochem@users.noreply.github.com>
2019-05-08 08:10:49 +0100

    create output storage

--- sim_analysis.r
+++ sim_analysis.r
@@ -1,6 +1,6 @@
 # 
 # Analysis code for simulated population surfaces
-# sim_analysis.r
+# sim_analysis.
 #
 #
 # Author: Chris Jochem (W.C.Jochem@soton.ac.uk)
@@ -18,7 +18,7 @@ r_width <- 20
 r_height <- 20
 # create spatial units 
 # make 5 horizontal strata (naive stratification for comparison)
-# see: https://gis.stackexchange.com/questions/34895/create-zonal-grid-in-r
+# see: https://gis.stackexchange.com/questions/34895/create-zonal-grid-in-
 rows <- 50; cols <- 200; n <- 200
 strat1 <- outer(1:n, 1:n, 
                function(i,j) (i-1) %/% rows * ((n+1) %/% cols) + (j-1) %/% cols + 1)
@@ -35,11 +35,17 @@ sampleszs <- c(50, 100, 150, 200) # range of sample sizes
 
 # get list of simulated surfaces
 sim_files <- list.files("./out_sim", pattern=glob2rx("sim_surface_*.rds"), recursive=FALSE, full.names=TRUE)
+# set up storage for results
+all_outputs <- matrix(NA,
+                      nrow=length(sim_files) * 50 * nsamples * 5 * length(sampleszs) * 4,
+                      ncol=10)
+counter <- 1 # index to store the results
 # loop and load all files
 for(f in sim_files){
   print(f)
   sim_surfaces <- readRDS(f) # load the simulated population and covariates
   nsurfaces <- length(sim_surfaces) # number of simulated populations
+  truerange <- as.numeric(gsub('^.*e_\\s*|\\s*.rds.*$', '', f)) # get range of spatial field
   
   # loop each simulated surface
   for(s in 1:nsurfaces){
@@ -47,6 +53,13 @@ for(f in sim_files){
     truepop <- sim_surfaces[[s]]$pop
     sett <- sim_surfaces[[s]]$sett
     cov <- sim_surfaces[[s]]$cov
+    
+    # loop sample size
+    for(sz in sampleszs){
+      # simple random sample
+      
+      
+    } # end sample size loop
     
   } # end loop of simulated surfaces
   
