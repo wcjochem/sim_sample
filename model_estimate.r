e788908a1cc620ccaff550ce1779c56039d9d030
wcjochem <7605755+wcjochem@users.noreply.github.com>
2019-05-08 16:09:40 +0100

    create file for model fitting functions

---
+++ model_estimate.r
@@ -0,0 +1,14 @@
+# 
+# Estimate a variety of simplified population models
+# model_estimate.r
+#
+# Helper functions for model-fitting
+#
+# Author: Chris Jochem (W.C.Jochem@soton.ac.uk)
+#
+
+# Model-based geostatistics
+mbg <- function(samp=NULL, pred=NULL, mesh=NULL){
+  
+  
+}
