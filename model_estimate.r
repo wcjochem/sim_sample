302d359458e591c67094a38d455117ef3fd03a3b
wcjochem <7605755+wcjochem@users.noreply.github.com>
2019-06-08 15:38:01 +0100

    return in-sample predictions

--- model_estimate.r
+++ model_estimate.r
@@ -13,7 +13,7 @@ mbg <- function(samp=NULL, pred_in=NULL, pred_out=NULL, mesh=NULL){
   # spde - spatial prior
   spde <- inla.spde2.pcmatern(mesh, prior.range=c(10, .9), prior.sigma=c(.5, .5))
   # components
-  form <- pop ~ -1 + Intercept + cov + f(sett, model="iid", values=1:5) + f(field, model=spde)
+  form <- pop ~ -1 + Intercept + cov + f(sett, model="iid") + f(field, model=spde)
   c.c <- list(cpo=TRUE, dic=TRUE, waic=TRUE, config=TRUE)
   # set up model
   A.est <- inla.spde.make.A(mesh=mesh,
@@ -32,28 +32,15 @@ mbg <- function(samp=NULL, pred_in=NULL, pred_out=NULL, mesh=NULL){
                           effects=list( c(mesh.index0, list(Intercept=1)),
                                         list(samp[,c("cov","sett")]) ),
                           tag='est')
-
-  # stack.pred_in <- inla.stack(data=list(pop=NA),
-  #                             A=list(A.pred_in, 1),
-  #                             effects=list( c(mesh.index0, list(Intercept=1)),
-  #                                           list(pred_in[,c("cov","sett")]) ),
-  #                             tag='pred_in')
-  # 
-  # stack.pred_out <- inla.stack(data=list(pop=NA),
-  #                              A=list(A.pred_out, 1),
-  #                              effects=list( c(mesh.index0, list(Intercept=1)),
-  #                                            list(pred_out[,c("cov","sett")]) ),
-  #                              tag='pred_out')
-    
-  # create unified stack with tags
-  # stack.all <- inla.stack(stack.est, stack.pred)
   # fit model
   fit <- tryCatch({
       inla(form, 
            family="poisson",
            data=inla.stack.data(stack.est),
            control.predictor=list(A=inla.stack.A(stack.est), compute=T, link=1),
-           control.compute=c.c, verbose=T)
+           control.compute=c.c 
+           # verbose=T
+          )
     },
     error=function(e){
       NULL
@@ -64,7 +51,7 @@ mbg <- function(samp=NULL, pred_in=NULL, pred_out=NULL, mesh=NULL){
     predvals <- NA
   } else{
     # predictions
-    nsamp <- 1e4
+    nsamp <- 1e3
     # draw samples from the posterior
     ps <- inla.posterior.sample(n=nsamp, fit)
     # get indices to the effects
@@ -85,6 +72,9 @@ mbg <- function(samp=NULL, pred_in=NULL, pred_out=NULL, mesh=NULL){
     xX <- xLatent[idX,]
     
     # construct predictions
+    # in-sample
+    linpred <- as.matrix(A.est %*% xSpace + xSett[samp$sett,] + as.matrix(cbind(1, samp[,c("cov")])) %*% xX)
+    pred_N_s <- t(apply(linpred, 1, FUN=function(x){ quantile(rpois(n=nsamp, lambda=exp(x)), probs=c(0.025,0.5,0.975)) }))
     # within sample area
     linpred <- as.matrix(A.pred_in %*% xSpace + xSett[pred_in$sett,] + as.matrix(cbind(1, pred_in[,c("cov")])) %*% xX)
     pred_N_in <- t(apply(linpred, 1, FUN=function(x){ quantile(rpois(n=nsamp, lambda=exp(x)), probs=c(0.025,0.5,0.975)) }))
@@ -93,6 +83,6 @@ mbg <- function(samp=NULL, pred_in=NULL, pred_out=NULL, mesh=NULL){
     pred_N_out <- t(apply(linpred, 1, FUN=function(x){ quantile(rpois(n=nsamp, lambda=exp(x)), probs=c(0.025,0.5,0.975)) }))
   }
   ## return
-  return(list("predN_in"=pred_N_in, "predN_out"=pred_N_out))
+  return(list("predN_s"=pred_N_s,"predN_in"=pred_N_in, "predN_out"=pred_N_out))
   
 }
